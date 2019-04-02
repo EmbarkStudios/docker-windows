357719695bee8f5666b0120a2d36dbb0ca3c026b
Jake Shadle <jake.shadle@embark-studios.com>
2019-04-02 15:22:01 +0200

    Add scoop Dockerfile

---
+++ scoop/install-scoop.ps1
@@ -0,0 +1,8 @@
+$ErrorActionPreference="Stop"
+
+echo "Temporarily allowing the execution of remote scripts"
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
+
+echo "Installing Scoop"
+Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh'))
+
