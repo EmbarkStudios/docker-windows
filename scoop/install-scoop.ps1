52971060bb2686d8f83325631d169aac1274842a
Jake Shadle <jake.shadle@embark-studios.com>
2019-04-09 16:55:44 +0200

    Try adding to PATH

--- scoop/install-scoop.ps1
+++ scoop/install-scoop.ps1
@@ -6,3 +6,4 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
 echo "Installing Scoop"
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh'))
 
+Add-Content -Path $Profile.CurrentUserAllHosts -Value '$Env:Path += ";C:\ContainerAdministrator\scoop\shims"'
