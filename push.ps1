8fbaaa417b7daa81deb3b1de0ff18c67558d27d7
Jake Shadle <jake.shadle@embark-studios.com>
2019-04-09 18:33:56 +0200

    Update images, add extras

--- push.ps1
+++ push.ps1
@@ -3,15 +3,25 @@ param (
     [string]$vs_tag = "2017"
 )
 
+# Stop and exit if any command/cmdlet fails
 $ErrorActionPreference="Stop"
 
 $base_tag = "ltsc2019"
 $org = "embarkstudios"
 
-$scoop_tag = "$org/scoop:$base_tag"
-$vs_tools_tag = "$org/vs-build-tools:$base_tag-$vs_tag"
-$rust_tag = "$org/rust:$base_tag-$rust_version"
-$total = 6
+$tags = @(
+  "$org/scoop:$base_tag",
+  "$org/vs-build-tools:$base_tag-$vs_tag",
+  "$org/rust:$base_tag-$rust_version",
+  "$org/rust-extras:$base_tag-$rust_version"
+)
+
+$dirs = @(
+    "scoop",
+    "vs-build-tools",
+    "rust",
+    "rust-extras"
+)
 
 function progress {
     param(
@@ -20,23 +30,28 @@ function progress {
         [int]$current = -1
     )
 
-    Write-Progress -Activity $act -Status $status -PercentComplete ($current/$total*100)
+    Write-Progress -Activity $act -Status $status -PercentComplete ($current/($tags.count * 2 + 1)*100)
 }
 
-progress -act "Building" -status $scoop_tag -current 1
-docker build -t $scoop_tag scoop
+function build {
+    for ($i = 0; $i -le ($tags.length - 1); $i += 1) {
+        progress -act "Building" -status $tags[$i] -current ($i + 1)
 
-progress -act "Building" -status $vs_tools_tag -current 2
-docker build -t $vs_tools_tag vs-build-tools
+        # HAAAAAAX!
+        if ($dirs[$i] -eq "rust") {
+            docker build -t $tags[$i] --build-arg "rust_version=$rust_version" $dirs[$i]
+        } else {
+            docker build -t $tags[$i] $dirs[$i]
+        }
+    }
+}
 
-progress -act "Building" -status $rust_tag -current 3
-docker build -t $rust_tag --build-arg "rust_version=$rust_version" rust
+function push {
+    for ($i = 0; $i -le ($tags.length - 1); $i += 1) {
+        progress -act "Pushing" -status $tags[$i] -current ($i + $tags.length + 1)
+        docker push $tags[$i]
+    }
+}
 
-progress -act "Pushing" -status $scoop_tag -current 4
-docker push $scoop_tag
-
-progress -act "Pushing" -status $vs_tools_tag -current 5
-docker push $vs_tools_tag
-
-progress -act "Pushing" -status $rust_tag -current 6
-docker push $rust_tag
+build
+push
---
+++ rust-extras/install-sccache.ps1
@@ -0,0 +1,19 @@
+param(
+    [string]$version = $env:SCCACHE_VERSION,
+    [string]$repo = $env:SCCACHE_REPO
+)
+
+$ErrorActionPreference="Stop";
+
+# We could alternatively also just do a cargo install <sccache-repo>
+$name = "sccache-$version-x86_64-pc-windows-msvc"
+
+Invoke-WebRequest -Uri "https://github.com/$repo/releases/download/$version/$name.tar.gz" -OutFile ./temp/archive.tar.gz
+
+# 7z apparently can't decompress and extract in one execution...why even
+7z e "-o./temp" ./temp/archive.tar.gz
+7z e "-o./temp" "./temp/$name.tar"
+ls ./temp
+
+# Place sccache in $HOME/.cargo/bin as it's in $PATH
+mv ./temp/sccache.exe $env:CARGO_HOME/bin
