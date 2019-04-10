18f64f29c91854bf63731828c2ed507a12f4a104
Jake Shadle <jake.shadle@embark-studios.com>
2019-04-10 15:52:46 +0200

    Trailing commans please ps

--- bk-build.ps1
+++ bk-build.ps1
@@ -1,7 +1,7 @@
 param (
     [string]$token = $(throw "-token is required."),
     [string]$tags = $(throw "-tags are required."),
-    [string]$tag = $(throw "-tag is required."),
+    [string]$tag = $(throw "-tag is required.")
 )
 
 echo "FROM embarkstudios/buildkite:ltsc2019-2017-1.33.0" | docker build -t $tag --build-arg "buildkiteAgentToken=$token" --build-arg "buildkiteAgentTags=$tags" -
