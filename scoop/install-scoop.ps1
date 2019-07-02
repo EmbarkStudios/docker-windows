e234fbcf6ce99d58e554223f473f883b73612085
Jake Shadle <jake.shadle@embark-studios.com>
2019-07-02 20:29:09 +0200

    Force scoop to update

--- scoop/install-scoop.ps1
+++ scoop/install-scoop.ps1
@@ -1,5 +1,5 @@
-echo "Temporarily allowing the execution of remote scripts"
+Write-Host "Temporarily allowing the execution of remote scripts"
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
 
-echo "Installing Scoop"
+Write-Host "Installing Scoop"
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh'))
