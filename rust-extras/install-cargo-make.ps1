ff730eacc39be52c5740d6c2f7ecc69acf627def
Jake Shadle <jake.shadle@embark-studios.com>
2020-02-25 14:35:58 +0100

    Update cargo-fetcher/sccache

--- rust-extras/install-cargo-make.ps1
+++
@@ -1,9 +0,0 @@
-param(
-    [string]$version = $(throw "-version is required.")
-)
-
-$name = "cargo-make-v$version-x86_64-pc-windows-msvc"
-
-Invoke-WebRequest -Uri "https://github.com/sagiegurari/cargo-make/releases/download/$version/$name.zip" -OutFile ./temp/archive.zip
-
-7z e ./temp/archive.zip "-o$env:CARGO_HOME/bin" "cargo-make.exe"
